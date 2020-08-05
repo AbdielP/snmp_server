ba69dc25ee81b51937832d65cd16c817b7850ddd
AbdielP <abdiel.pinzonc@gmail.com>
2020-08-05 13:29:12 -0500

    Se agrega función para evitar IP se sensores duplicados

--- routes/sensores.js
+++ routes/sensores.js
@@ -10,17 +10,20 @@ app.post('/agregar/sensor',function(req,res){
             return console.log(err)
         }
         var sensores = JSON.parse(data)
+        var filter = sensores.sensores.filter(obj => obj.ip === newSensor.ip)
+        console.log(filter)
+        if(filter != ""){
+            console.log('Es diferente de []')
+            return res.status(500).json({ok:false,message:`Sensor ${body.ip} ya existe.`})
+        }
         sensores.sensores.push(newSensor)
         var str_sensores = JSON.stringify(sensores)
-        // var sensores_sort = sensores.sensores.sort(function(a,b){
-        //     return a - b;
-        // });
         fs.writeFile(`./files/${body.archivo}.txt`,str_sensores,'utf8',function(err,results){
             if(err) return console.log(err)
             return res.status(200).json({ok:true,sensores,message:`Sensor ${body.ip} agregado en ${body.planta}`})
         })
     })
-})
+});
 
 app.delete('/remover/sensor/:archivo/:ip',function(req,res){
     var params = req.params;
@@ -37,6 +40,6 @@ app.delete('/remover/sensor/:archivo/:ip',function(req,res){
             return res.status(200).json({ok:true,obj})
         })
     })
-})
+});
 
 module.exports = app;
